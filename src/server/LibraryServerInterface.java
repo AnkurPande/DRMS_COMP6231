3bf38a979aa0a6afc5e9db50cc19ee1f6cc1b6b1
shyqqtt <shyqqtt@gmail.com>
2015-05-24 01:29:48 -0400

    Declare server methods

--- src/server/LibraryServerInterface.java
+++ src/server/LibraryServerInterface.java
@@ -1,5 +1,49 @@
 package server;
 
-public interface LibraryServerInterface {
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 
+/**
+ * This is the server interface.
+ * 
+ * @author Haiyang Sun
+ *
+ */
+
+public interface LibraryServerInterface extends Remote {
+	
+	/**
+	 * 
+	 * @param firstName
+	 * @param lastName
+	 * @param emailAddress
+	 * @param phoneNumber
+	 * @param username
+	 * @param password
+	 * @param eduInstitution
+	 * @return
+	 * @throws RemoteException
+	 */
+	public boolean createAccount(String firstName, String lastName, String emailAddress,
+			String phoneNumber, String username, String password, String eduInstitution) throws RemoteException;
+	
+	/**
+	 * 
+	 * @param username
+	 * @param password
+	 * @param bookName
+	 * @param authorName
+	 * @return
+	 * @throws RemoteException
+	 */
+	public boolean reserveBook(String username, String password, String bookName, String authorName) throws RemoteException;
+	
+	/**
+	 * 
+	 * @param adminUsername
+	 * @param adminPassword
+	 * @return
+	 * @throws RemoteException
+	 */
+	public String getNonRetuners(String adminUsername, String adminPassword) throws RemoteException;
 }
