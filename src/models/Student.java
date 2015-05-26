a6aa25a630f94c3cd8a89855fa34b53bf774fb72
Syed Sumair Zafar <sumairz@gmail.com>
2015-05-26 14:10:45 -0400

    Book Model completed
    Book models is completed + more functionality will be added as per need

--- src/models/Student.java
+++ src/models/Student.java
@@ -27,7 +27,7 @@ public class Student {
 	private Map<String, Integer> books = new HashMap<String, Integer>();
 	
 	// filename to save the student data
-	private String filename = null; 
+	//private String filename = null; 
 		
 	
 	/*
@@ -85,5 +85,178 @@ public class Student {
 		this.finesAccumulated = 0;
 	}
 	
+	
+	// ------------------------ GETTERS Functions START --------------------------------------
+	/**
+	 * This function will return student's ID
+	 */
+	public String getStudent_id() 
+	{
+		return this.studentID;
+	}
+	
+	/**
+	 * This funciton will return student's first name
+	 */
+	public String getFirstName() 
+	{
+		return this.firstName;
+	}
+	
+	
+	/**
+	 * This function will return student's last name
+	 */
+	public String getLastName() 
+	{
+		return this.lastName;
+	}
+	
+	
+	/**
+	 * This function will return student's full name
+	 */
+	public String getFullName() 
+	{
+		return this.firstName + this.lastName;
+	}
+	
+	
+	/**
+	 * This function will return student's email address
+	 */
+	public String getEmailAddress() 
+	{
+		return this.emailAddress;
+	}
+	
+	
+	/**
+	 * This function will return student's phone number
+	 */
+	public String getPhoneNumber() 
+	{
+		return this.phoneNumber;
+	}
+	
+	/**
+	 * This function will return student's username
+	 */
+	public String getUserName() 
+	{
+		return this.userName;
+	}
+	
+	/**
+	 * This function will return student's password
+	 */
+	public String getPassword() 
+	{
+		return this.password;
+	}
+	
+	/**
+	 * This function will return student's institute
+	 */
+	public String getEducationalIns() 
+	{
+		return this.eduInstitute;
+	}
+	
+	/**
+	 * This function will return fines
+	 */
+	public double getFinesAccumulated() 
+	{
+		return this.finesAccumulated;
+	}
+	
+	
+	public Map<String, Integer> getBooks() 
+	{
+		return books;
+	}
+	
+	// ------------------------ GETTERS Functions END --------------------------------------
+	
+	
+	
+	// ------------------------ SETTERS Functions Start --------------------------------------
+	
+	public void setStudent_id(String studentID) 
+	{
+		this.studentID = studentID;
+	}
+	
+	
+	public void setFirstName(String firstName) 
+	{
+		this.firstName = firstName;
+	}
+	
+	public void setLastName(String lastName) 
+	{
+		this.lastName = lastName;
+	}
+	
+	public void setEmailAddress(String emailAddress)
+	{
+		this.emailAddress = emailAddress;
+	}
+	
+	public void setPhoneNumber(String phoneNumber) 
+	{
+		this.phoneNumber = phoneNumber;
+	}
+	
+	public void setUserName(String userName) 
+	{
+		this.userName = userName;
+	}
+	
+	public void setPassword(String password) 
+	{
+		this.password = password;
+	}
+	
+	public void setEducationalIns(String eduInstitute) 
+	{
+		this.eduInstitute = eduInstitute;
+	}
+	
+	public void setFinesAccumulated(double finesAccumulated)
+	{		
+		this.finesAccumulated = finesAccumulated;
+	}	
+	
+	public void setBooks(Map<String, Integer> books) 
+	{
+		this.books = books;
+	}
+	
+	// ------------------------ SETTERS Functions END --------------------------------------
+	
+	
+	// ------------------------ Misc Functions --------------------------------------------
+	
+	/*
+	 * This function will automatically add daily fines to student who didn't returned books after deadline
+	 */
+	public void addDailyFine() 
+	{		
+		this.finesAccumulated += 1;
+	}
+	
+	
+	/*
+	 * This function will reserve a book
+	 */
+	public void reserveBook(String book_id, int duration)
+	{	
+		if(duration == 0)
+			duration = 14; // default duration for a book if none is provided
+		
+		this.books.put(book_id, duration);
+	}
 	
 }
