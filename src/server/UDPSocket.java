001b35dea4b6a7a5dc52e09ba3732b485fbfeb54
Ankur Pandey <ankur.pandey111@gmail.com>
2015-05-26 02:32:49 -0400

    Update UDPSocket.java

--- src/server/UDPSocket.java
+++ src/server/UDPSocket.java
@@ -1,5 +1,41 @@
 package server;
+import java.net.*;
 
 public class UDPSocket {
-
+	DatagramSocket socket =null;
+	private int udpPort = 0;
+	String data;
+	public int getUdpPort() {
+		return udpPort;
+	}
+	
+	public void sendResponse(byte[] buf, int length, InetAddress address, int port){
+		try {
+		socket = new DatagramSocket(udpPort);
+		DatagramPacket reply = new DatagramPacket(buf,buf.length,address,port);
+		socket.send(reply);
+		} catch (Exception e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		}
+		finally{
+			socket.close();
+		}
+	}
+	
+	public String recieveRequest(byte buf[],int udpPort){
+		try {
+			socket = new DatagramSocket(udpPort);
+			DatagramPacket recievedPacket = new DatagramPacket(buf,buf.length);
+			socket.receive(recievedPacket);
+			data = new String(recievedPacket.getData());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		finally{
+			socket.close();
+		}
+		return data;
+	}
 }
