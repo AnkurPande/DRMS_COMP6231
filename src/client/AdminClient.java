52b15d06e81eed8cd2730acc920b92ac1b3ca6b3
Mitul <Mitul@Mitul-PC.wireless.concordia.ca>
2015-05-26 15:58:50 -0400

    First implimentation of Admin Client

--- src/client/AdminClient.java
+++ src/client/AdminClient.java
@@ -1,5 +1,22 @@
 package client;
 
+/**
+ * <h1>Admin Client</h1>
+ * <p>
+ * 		This File is use for Admin Client to compare the Actual Admin from database 
+ * 		and its seem like fecade of the Library System
+ * </p>
+ * 
+ * @version 1.0.0.0
+ * @author Mitul
+ */
 public class AdminClient {
+	
+	private String AdminClientName;
+	private String AdminPasword;
+	private String EDU_Institution;
+	private int NumberofDay;
 
+	
+	
 }
