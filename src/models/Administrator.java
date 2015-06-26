212e9ced5dd3163b8d309913feeaf2df6272d1ee
shyqqtt <shyqqtt@gmail.com>
2015-06-26 19:50:53 -0400

    java doc

--- src/models/Administrator.java
+++ src/models/Administrator.java
@@ -6,19 +6,49 @@
 
 package models;
 
+/**
+ * The Class Administrator.
+ */
 public class Administrator {
 	
+	/** The admin username. */
 	private String adminUsername;
+	
+	/** The admin password. */
 	private String adminPassword;
+	
+	/**
+	 * Gets the admin username.
+	 *
+	 * @return the admin username
+	 */
 	public String getAdminUsername() {
 		return adminUsername;
 	}
+	
+	/**
+	 * Sets the admin username.
+	 *
+	 * @param adminUsername the new admin username
+	 */
 	public void setAdminUsername(String adminUsername) {
 		this.adminUsername = adminUsername;
 	}
+	
+	/**
+	 * Gets the admin password.
+	 *
+	 * @return the admin password
+	 */
 	public String getAdminPassword() {
 		return adminPassword;
 	}
+	
+	/**
+	 * Sets the admin password.
+	 *
+	 * @param adminPassword the new admin password
+	 */
 	public void setAdminPassword(String adminPassword) {
 		this.adminPassword = adminPassword;
 	}
--- src/models/LibraryServerInfo.java
+++ src/models/LibraryServerInfo.java
@@ -1,29 +1,64 @@
 package models;
 
+/**
+ * The Class LibraryServerInfo.
+ */
 public class LibraryServerInfo {
 	
+	/** The port of udp. */
 	private int portOfUDP;
+	
+	/** The server name. */
 	private String serverName;
 	
+	/**
+	 * Instantiates a new library server info.
+	 *
+	 * @param name the name
+	 * @param port the port
+	 */
 	public LibraryServerInfo(String name, int port) {
 		this.serverName = name;
 		this.portOfUDP = port;
 	}
 	
+	/**
+	 * Instantiates a new library server info.
+	 */
 	public LibraryServerInfo() {};
 	
+	/**
+	 * Gets the port of udp.
+	 *
+	 * @return the port of udp
+	 */
 	public int getPortOfUDP() {
 		return portOfUDP;
 	}
 	
+	/**
+	 * Sets the port of udp.
+	 *
+	 * @param portOfUDP the new port of udp
+	 */
 	public void setPortOfUDP(int portOfUDP) {
 		this.portOfUDP = portOfUDP;
 	}
 	
+	/**
+	 * Gets the server name.
+	 *
+	 * @return the server name
+	 */
 	public String getServerName() {
 		return serverName;
 	}
 	
+	/**
+	 * Sets the server name.
+	 *
+	 * @param serverName the new server name
+	 */
 	public void setServerName(String serverName) {
 		this.serverName = serverName;
 	}
