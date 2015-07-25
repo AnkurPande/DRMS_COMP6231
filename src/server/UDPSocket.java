648b694e4439549b4d0570efcd0e841fe232b250
Ankur Pandey <ankur.pandey111@gmail.com>
2015-07-25 18:02:42 -0400

    Update UDPSocket.java

--- src/server/UDPSocket.java
+++ src/server/UDPSocket.java
@@ -3,79 +3,173 @@ package server;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketTimeoutException;
+
+import model.DatagramWrapper;
 
 /**
  * @author Ankurp
  *
  */
 public class UDPSocket extends Thread {
-	
+	private DatagramSocket DGSock = null;
+	private int port = 0;
+	private int bytesRead = 0;
+	private int bookmark = 0;
+	private static final int MAX_SIZE = 80;
+	byte[] bbuff = null;
+	byte[] bytes = null;
+	String response = "";
+	String request = null;
+	long responseSize = 0;
+	DatagramWrapper lastReceived = new DatagramWrapper();
+	DatagramWrapper lastSent = new DatagramWrapper();;
+	DatagramPacket packetDock = null;
+
 	private LibraryServer server;
-	
-	public UDPSocket(LibraryServer server) {
-		this.server = server;
-		
+				
+	public DatagramSocket getDGSock() {
+		return DGSock;
 	}
+	public int getPort() {
+		return port;
+	}
+	public String getRequest() {
+		return request;
+	}
+	public void setDGSock(DatagramSocket dGSock) {
+		DGSock = dGSock;
+	}
+	public void setPort(int port) {
+		this.port = port;
+	}
+	public void setRequest(String request) {
+		this.request = request;
+	}
+
+	public UDPSocket(){}
+	
+	//Initialization
+	public UDPSocket(LibraryServer server){
+		//usage checking
+    	try {	
+    			this.server = server;
+    			this.port = server.getPortOfUDP();
+    			
+    		} catch ( Exception e ) {
+    			System.out.println(e.getMessage());
+    			System.exit(-1);
+    		}
+    	System.out.println("Server Started at port"+port);
+	}
+	    	
 	@Override
 	public void run() {
-		DatagramSocket socket = null;
-		String responseMessageString = "";
-		try {
+		//setup
+    	try {
+    	    bbuff = new byte[1000];
+    	    DGSock = new DatagramSocket(port);
+    	    packetDock = new DatagramPacket(bbuff, bbuff.length);
+    	    
+    	    try {
+    	    	DGSock.receive(packetDock);
+	    		DGSock.setSoTimeout(1000);
+	    		lastReceived = DatagramWrapper.fromDatagram(packetDock);
+	    		request = new String(lastReceived.getPayload());
+	       	} catch ( SocketTimeoutException e ) {
+		    	System.out.println("Socket Timeout Exception while Recieve");
+		    } catch ( Exception e ) {
+    	    	System.out.println("Received bad request!\nTerminating program...");
+    	    	System.out.println(e.getMessage());
+    	    	System.exit(-1);
+    	    }
+    	    
+    	    String[] requestParts = request.split(",");
 			
-			
-			socket = new DatagramSocket(server.getPortOfUDP(), InetAddress.getByName(server.getIpAddress()));
-			byte[] buffer = new byte[1000];
-			
-			while(true){
-				DatagramPacket requestPacket = new DatagramPacket(buffer, buffer.length);
-				socket.receive(requestPacket);
-				
-				byte[] message = requestPacket.getData();
-				String receivedMessageString = new String(message).trim();
-				
-				String[] requestParts = receivedMessageString.split(",");
-				
-				if(requestParts.length == 2 ) {
-					//non return request
-					String numDays = requestParts[1].trim();
-					responseMessageString = server.checkNonRetuners(numDays);
+			if(requestParts.length == 2 ) {
+				//non return request
+				String numDays = requestParts[1].trim();
+				response = server.checkNonRetuners(numDays);
+			}
+			else {
+				/*
+				 * reserve request
+				 * 
+				 * Prefix Definition
+				 * 1 -- check book availability
+				 * 2 -- release book
+				 * 3 -- confirm remote reservation
+				 */
+				if(requestParts[0].trim().equalsIgnoreCase("1")) {
+					response = server.checkBookAvailability(requestParts[1].trim(),requestParts[2].trim())?"true":"false";
 				}
-				else {
-					/*
-					 * reserve request
-					 * 
-					 * Prefix Definition
-					 * 1 -- check book availability
-					 * 2 -- release book
-					 * 3 -- confirm remote reservation
-					 */
-					if(requestParts[0].trim().equalsIgnoreCase("1")) {
-						responseMessageString = server.checkBookAvailability(requestParts[1].trim(),requestParts[2].trim())?"true":"false";
-					}
-					if(requestParts[0].trim().equalsIgnoreCase("2")) {
-						server.releaseBook(requestParts[1].trim(),requestParts[2].trim());
-						responseMessageString = "true";
+				if(requestParts[0].trim().equalsIgnoreCase("2")) {
+					server.releaseBook(requestParts[1].trim(),requestParts[2]);
+					response = "true";
 
-					}
-					if(requestParts[0].trim().equalsIgnoreCase("3")) {
-
-						server.confirmRemoteReservation(requestParts[1].trim(),requestParts[2].trim());
-						responseMessageString = "true";
-					}
-					
 				}
-				
-				message = responseMessageString.getBytes();
-				
-				DatagramPacket responsePacket = new DatagramPacket(message, message.length, requestPacket.getAddress(), requestPacket.getPort());
-				socket.send(responsePacket);
+				if(requestParts[0].trim().equalsIgnoreCase("3")) {
+
+					server.confirmRemoteReservation(requestParts[1].trim(),requestParts[2].trim());
+					response = "true";
+				}
 			}
 			
+    	    long offset = 0;
+    	    responseSize = response.length();
+    	
+    	    //traffic loop
+		    while ( lastReceived.getEnd() < responseSize ) {
 			
-		} catch (Exception e) {
-			System.out.println("UDP Exception");
-		} finally {
-			if (socket != null) socket.close();
-		}
+			//create next packet to send		
+			bytes = new byte[MAX_SIZE+32];
+			offset = responseSize -lastReceived.getEnd();
+			if(offset>=MAX_SIZE) {
+				bytes = response.substring(bookmark, MAX_SIZE).getBytes();
+				bytesRead = response.substring(bookmark, MAX_SIZE).length();
+			}
+			else{
+				bytes = response.substring(bookmark, response.length()-1).getBytes();
+				bytesRead = response.length() -bookmark ;
+			} 
+			
+			
+			lastSent = new DatagramWrapper(bookmark, bookmark+bytesRead, bytes,
+										lastReceived.asDatagram().getAddress(),
+										lastReceived.asDatagram().getPort());
+	
+			//guarantee delivery
+			while (true) {
+			    try {
+				DGSock.send(lastSent.asDatagram());
+				DGSock.receive(packetDock);
+				lastReceived = DatagramWrapper.fromDatagram(packetDock);
+				if ( lastReceived.getEnd() < lastSent.getEnd() ) continue;
+				break;
+			    } catch ( SocketTimeoutException e ) {
+			    	System.out.println("Socket Timeout Exception while Send response OR recieving ACK.");
+			    } catch ( Exception e ) {
+			    	System.out.println(e.getMessage());
+			    }
+			}
+			if ( (bookmark < responseSize) && (bytesRead > 0) ) bookmark += bytesRead;
+		    }
+    	}
+    	catch(Exception e){
+    		System.out.println("Exception while Sending response: "+e.getMessage());
+   		}
+    	
+    	//FIN cycle
+    	lastSent = new DatagramWrapper(-1, -1, lastReceived.asDatagram().getAddress(), lastReceived.asDatagram().getPort());
+    	System.out.println("Sending FINS. Will terminate momentarily.");
+    	
+    	for ( int i=0; i<20; i++ ) {
+    	   try {
+    			DGSock.send(lastSent.asDatagram());
+    		} catch ( SocketTimeoutException e ) {
+    		    	System.out.println("Socket Timeout Exception While Sending FINS.");
+    		} catch ( Exception e ) { e.printStackTrace(); }
+    	}    
+    	if (DGSock != null) DGSock.close();
 	}
 }
