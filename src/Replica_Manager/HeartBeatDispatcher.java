8ae13bf93c6ef56e77617addbcefc278d9b8e7f0
s_zaf <s_zaf@SWITZERLAND.ENCS.concordia.ca>
2015-08-07 12:53:39 -0400

    basic structure for heartbeat messages (incompleted)

--- src/Replica_Manager/HeartBeatDispatcher.java
+++ src/Replica_Manager/HeartBeatDispatcher.java
@@ -1,5 +1,45 @@
 package Replica_Manager;
 
-public class HeartBeatDispatcher {
+import java.util.Map;
+import java.util.Set;
 
+import server.UDPSender;
+import server.UDPSocket;
+
+public class HeartBeatDispatcher implements Runnable {
+
+	public static int SLEEP_TIME = 5000;
+	
+	private final ReplicaManager rm;
+	private final Map<Integer,Integer> REPLICA_MANAGER_PORTS;
+	private final Map<Integer,String> REPLICA_MANAGER_IPS;
+	
+	public HeartBeatDispatcher(final ReplicaManager rm, final Map<Integer,Integer> rmPorts, final Map<Integer,String> rmIps) {
+		this.rm = rm;
+		this.REPLICA_MANAGER_PORTS = rmPorts;
+		this.REPLICA_MANAGER_IPS = rmIps;
+	}
+	
+	@Override
+	public void run() {
+		try {
+			while (true) {
+				Thread.sleep(SLEEP_TIME);				
+							
+			}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	/**
+	 * Send heart beat to check if library in another replica manager is alive. The response will be handled by this.rm.
+	 * @param rmId
+	 * @param port
+	 * @return Nothing
+	 */
+	public void dispatchHeartBeat(String rmId, int port) {
+		
+	}
 }
--- src/Replica_Manager/ReplicaManager.java
+++ src/Replica_Manager/ReplicaManager.java
@@ -1,5 +1,85 @@
 package Replica_Manager;
 
-public class ReplicaManager {
+import java.util.HashMap;
 
+public class ReplicaManager {
+		
+	public final int CURRENT_RM_ID = 1;
+	
+	HashMap<Integer, Integer> rmPorts = new HashMap<Integer, Integer>();
+	HashMap<Integer, String> rmIps = new HashMap<Integer, String>();
+
+	
+	/* CONSTRUCTOR */
+	public ReplicaManager() {
+		
+		this.replicaManagerValues();
+	}
+	
+	
+	/**
+	 * This method will dump replica managers ports and IP values in hashmaps
+	 *
+	 * @param nothing
+	 * @return nothing
+	 */
+	private void replicaManagerValues()
+	{
+		rmPorts.put(1, 5001);
+		rmPorts.put(2, 5002);
+		rmPorts.put(3, 5003);
+		
+		rmIps.put(1, "localhost");
+		rmIps.put(2, "localhost");
+		rmIps.put(3, "localhost");
+	}
+	
+	
+	/**
+	 * This method will get the current Replica Manager's ID
+	 *
+	 * @param nothing
+	 * @return CURRENT_RM_ID
+	 */
+	public int getReplicaManagerID() {
+		return this.CURRENT_RM_ID;		
+	}
+	
+	
+	/**
+	 * This method will start a thread to send heart beat messages to Replica Managers
+	 *
+	 * @param 
+	 * @return nothing
+	 */
+	public void startHeartBeatProcess()	{
+		HeartBeatDispatcher dispatcher = new HeartBeatDispatcher(this, this.rmPorts, this.rmIps);
+		Thread t = new Thread(dispatcher);
+		t.start();
+	}
+	
+	
+	/**
+	 * This method will receive a response from heart beat response and take action according to response
+	 *
+	 * @param 
+	 * @return a string value
+	 */
+	public String handleHeartBeatResponse()	{
+		return null;
+		
+	}
+	
+	/**
+	 * This method will recover a dead replica manager.
+	 *
+	 * @param RmID: Replica Manager ID
+	 * @return a boolean indicate success or not
+	 */
+	public boolean recoverReplicaManager(int RmID)
+	{
+		return true;
+		
+	}
+	
 }
